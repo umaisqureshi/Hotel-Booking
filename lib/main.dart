4102dfb509be1697790cb5adf4f6be53630dc38c
Umais Qureshi <umaisqureshi26@icloud.com>
2024-09-23 10:25:22 +0200

    Fianl commit - Restrict app to portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,23 @@
 import 'dart:io';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:hotel_booking/domain/hotels/hotel_entity.dart';
 import 'package:hotel_booking/infrastucture/adapters/hive_adapters.dart';
 import 'package:hotel_booking/infrastucture/app_dependency.dart';
 import 'package:hotel_booking/infrastucture/localization/locale.dart';
 import 'package:hotel_booking/infrastucture/routing/app_router.dart';
-import 'package:path_provider/path_provider.dart' as path;
+import 'package:hotel_booking/presentation/presentation.dart';
 import 'package:easy_localization_loader/easy_localization_loader.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await createDependencyGraph();
   await EasyLocalization.ensureInitialized();
-  Directory directory = await path.getApplicationDocumentsDirectory();
-  await Hive.initFlutter(directory.path);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   await HiveAdapters().initAdapters();
   await Hive.openBox<Hotel>("FavoriteBox");
 
